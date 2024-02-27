bc2abd60e81cdc7d113837a2f1bbe383c1f8d506
mwihoti <danielmwihoti@gmail.com>
2024-02-27 05:08:19 -0800

    Text me script Receiver correction

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
-
 SENDER = ARGV[0].scan(/from:\+*\w*/).join[5..-1]
-RECEIVER = ARGV[0].scan(/to:\+*\w*/).join[5..-1]
+RECEIVER = ARGV[0].scan(/to:\+*\w*/).join[3..-1]
 FLAGS = ARGV[0].scan(/flags:(.*?)\]/).join
+
 txt = SENDER + "," + RECEIVER + "," + FLAGS
 puts txt
