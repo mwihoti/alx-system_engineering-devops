b57a9e754032e45b9d63b523d86cce51b0c432a2
mwihoti <danielmwihoti@gmail.com>
2024-04-19 10:26:32 -0700

    Create file using puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# with puppet create a file in /tmp
+file { '/tmp/school':
+  ensure  => present,
+  path    => '/tmp/school/',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
