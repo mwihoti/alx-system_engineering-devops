9b968dfff4f92d7c9d58aeb5960302b1a01c5f5a
mwihoti <danielmwihoti@gmail.com>
2024-06-05 04:05:18 +0300

    get subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,16 +14,14 @@ def number_of_subscribers(subreddit):
     """
     reddit_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    headers = {'User-Agent': 'my-user-agent'}
+    headers = {'User-Agent': 'Mozilla/5.0'}
 
-    try:
-
-        response = requests.get(reddit_url, headers=headers,
+    response = requests.get(reddit_url, headers=headers,
                                 allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
+    if response.status_code == 200:
+        data = response.json()
+        subs = data['data']['subscribers']
+        return subs
+    else:
             return 0
-    except requests.RequestException:
-        return 0
+   
